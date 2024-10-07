c0084e5687691080b6c8b8d3e213dfcf7a11a2fd
nachochiappero <ignaciogabrielchiappero@gmail.com>
2024-10-06 21:45:27 -0300

    arreglo texto escucha mis podcast y franja inferior

--- src/app/(pages)/podcast/page.jsx
+++ src/app/(pages)/podcast/page.jsx
@@ -5,11 +5,11 @@ import App from './swiper/App'
 
 const Portfolio = () => {
   return (
-    <div className='h-screen'>
+    <div className='mb-20'>
       <div className="mt-20 flex-col items-center justify-center text-center">
         
-        <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-6xl lg:leading-normal font-extrabold">
-            <span className="text-transparent bg-clip-text bg-gradient-to-r  text-white">
+        <h1 className=" mb-4 text-4xl sm:text-5xl lg:text-6xl lg:leading-normal font-extrabold">
+            <span className=" text-secondary-500">
               Escuchá mis podcast!
             </span>
         </h1>
